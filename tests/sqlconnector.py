552724ac20fca50fa4630f34dc828fd72d17f313
nconnel4 <nconnel4@hotmail.com>
2021-07-14 18:49:53 -0400

    Convert test_connect_db to sqlalchemy

--- tests/sqlconnector.py
+++ tests/sqlconnector.py
@@ -5,17 +5,15 @@ import sqlite3
 from covidvaccinationproject.util import sqlconnector as sc
 
 from covidvaccinationproject.util.logger import logconfig
-from covidvaccinationproject.util.logger import logconfig
 
 logconfig.setup_logging()
-logging.getLogger('covidvacinationproject.util.sqlconnector').setLevel(logging.DEBUG)
+logging.getLogger('covidvaccinationproject.util.sqlconnector').setLevel(logging.DEBUG)
 
 
 def test_connect_db():
-    conn = sc.connect_db()
-
     try:
-        conn.cursor()
+        conn = sc.connect_db()
+        conn.connect()
         connection_success = True
     except Exception:
         connection_success = False
