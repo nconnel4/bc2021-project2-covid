4abf3cf489bd99965a2cdbf875363064ceccee92
nconnel4 <nconnel4@hotmail.com>
2021-07-14 19:24:22 -0400

    Add test for create_table function in SqlConnector

--- tests/sqlconnector.py
+++ tests/sqlconnector.py
@@ -1,8 +1,8 @@
 import pytest
 import logging
-import sqlite3
+from sqlalchemy import Column, String, Integer, inspect
 
-from covidvaccinationproject.util import sqlconnector as sc
+from covidvaccinationproject.util.sqlconnector import SqlConnector
 
 from covidvaccinationproject.util.logger import logconfig
 
@@ -10,13 +10,31 @@ logconfig.setup_logging()
 logging.getLogger('covidvaccinationproject.util.sqlconnector').setLevel(logging.DEBUG)
 
 
+@pytest.fixture
+def connector():
+    return SqlConnector()
+
+
 def test_connect_db():
+    connector = SqlConnector()
+
     try:
-        conn = sc.connect_db()
+        conn = connector._connect_engine()
         conn.connect()
         connection_success = True
     except Exception:
         connection_success = False
 
     assert connection_success
+
+
+def test_create_table(connector):
+    table_schema = {'__tablename__': 'test_table',
+                    'test_col_1': Column(Integer, primary_key=True),
+                    'test_col_2': Column(String)}
+
+    connector.create_table(table_schema)
+
+    assert inspect(connector.engine).has_table('test_table')
+
 
