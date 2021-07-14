ec12767514153c7f6cf3a25e7c4a5581d7752990
nconnel4 <nconnel4@hotmail.com>
2021-07-14 19:24:52 -0400

    Add create_table function

--- covidvaccinationproject/util/sqlconnector.py
+++ covidvaccinationproject/util/sqlconnector.py
@@ -1,20 +1,31 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
 import logging
 from pathlib import Path
 
 
-def connect_db():
-    # instantiate logger
-    logger = logging.getLogger(__name__)
+class SqlConnector:
 
-    # get flask root level folder
-    root_path = Path(__file__).parent.parent
-    db_path = root_path / 'data/covid.db'
+    def __init__(self):
+        self._logger = logging.getLogger(__name__)
 
-    logger.info('Connecting to database')
-    try:
-        engine = create_engine(f'sqlite:///{db_path}')
-    except Exception as ex:
-        logger.error(ex)
+        self.engine = self._connect_engine()
 
-    return engine
+    def _connect_engine(self):
+        # get flask root level folder
+        root_path = Path(__file__).parent.parent
+        db_path = root_path / 'data/covid.db'
+
+        self._logger.info('Connecting to database')
+        try:
+            engine = create_engine(f'sqlite:///{db_path}')
+        except Exception as ex:
+            self._logger.error(ex)
+
+        return engine
+
+    def create_table(self, table_schema):
+        Base = declarative_base()
+
+        table = type('Table', (Base, ), table_schema)
+        table.__table__.create(bind=self.engine)
