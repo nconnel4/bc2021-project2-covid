0e580daa45d81a90af261abbd21178d30d4febb5
nconnel4 <nconnel4@hotmail.com>
2021-07-14 18:19:20 -0400

    Add exception if page not found

--- covidvaccinationproject/util/dataconnector.py
+++ covidvaccinationproject/util/dataconnector.py
@@ -1,8 +1,20 @@
 import requests
 import json
+import logging
+
+
+class InvalidSiteError(Exception):
+    """Exception raised if status code returned is 404"""
+    pass
 
 
 def get_json_from_web(url):
+    # instantiate logger
+    logger = logging.getLogger(__name__)
+
     r = requests.get(url)
+
+    if r.status_code == 404:
+        raise InvalidSiteError
 
     return r.json()
