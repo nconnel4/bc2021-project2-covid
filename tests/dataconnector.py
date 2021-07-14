5cffd03332bc34cbddf9dd4f16042557d1888796
nconnel4 <nconnel4@hotmail.com>
2021-07-14 18:17:16 -0400

    Add invalid site test

--- tests/dataconnector.py
+++ tests/dataconnector.py
@@ -1,6 +1,12 @@
+import logging
+
 import pytest
 
 from covidvaccinationproject.util import dataconnector as dc
+from covidvaccinationproject.util.logger import logconfig
+
+logconfig.setup_logging()
+logging.getLogger('covidvacinationproject.util.dataconnector').setLevel(logging.DEBUG)
 
 
 def test_connect_web_json():
@@ -8,3 +14,9 @@ def test_connect_web_json():
     data = dc.get_json_from_web(url)
 
     assert data is not None
+
+
+def test_invalid_site_error():
+    url = 'http://httpbin.org/status/404'
+    with pytest.raises(dc.InvalidSiteError):
+        data = dc.get_json_from_web(url)
