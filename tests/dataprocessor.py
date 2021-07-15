19fd94e4302be56c61de2b1b763d89adef5ad3b5
nconnel4 <nconnel4@hotmail.com>
2021-07-15 14:16:02 -0400

    Add test for extract_country_list

--- tests/dataprocessor.py
+++ tests/dataprocessor.py
@@ -1,6 +1,15 @@
 import pytest
 import logging
 
+from covidvaccinationproject.util import dataprocessor as dp
+
 from covidvaccinationproject.util.logger import logconfig
 logconfig.setup_logging()
 logging.getLogger('covidvaccinationproject.util.dataprocessor').setLevel(logging.DEBUG)
+
+def test_country_list():
+    country_list = dp.extract_country_list()
+
+    assert country_list[0]['country_id'] == 'AFG'
+    assert country_list[2]['location'] == 'Albania'
+    assert len(country_list) == 218
