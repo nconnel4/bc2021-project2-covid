36c43e066402d8ca0533c5706165d5e902d1b355
nconnel4 <nconnel4@hotmail.com>
2021-07-15 17:39:23 -0400

    Add test for covid_data api

--- tests/webservice.py
+++ tests/webservice.py
@@ -9,10 +9,12 @@ from covidvaccinationproject.util.logger import logconfig
 logconfig.setup_logging()
 logging.getLogger('covidvaccinationproject.util.sqlconnector').setLevel(logging.DEBUG)
 
+
 def test_country_list():
     country_list = webservice.get_country_list()
 
     assert country_list[0] == {'country_id': 'AFG', 'country': 'Afghanistan'}
+
 
 def test_country_metric():
     country_metric_list = webservice.get_country_demographics()
@@ -23,3 +25,11 @@ def test_country_metric():
 
     assert len(country_metric_list_filtered) == 1
     assert country_metric_list[0]['country_id'] == 'AFG'
+
+
+def test_covid_data():
+    covid_data = webservice.get_covid_data('MEX')
+    assert covid_data[0]['country_id'] == 'MEX'
+
+    covid_data = webservice.get_covid_data(start_date='2020-12-30', end_date='2020-12-32')
+    assert len(covid_data) > 1
