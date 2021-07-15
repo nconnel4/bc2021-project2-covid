5ca9233a6b972997af4e8841143c93f47d141f93
nconnel4 <nconnel4@hotmail.com>
2021-07-15 11:43:32 -0400

    Refactor to use seperate Table class

--- tests/sqlconnector.py
+++ tests/sqlconnector.py
@@ -2,7 +2,7 @@ import pytest
 import logging
 from sqlalchemy import Column, String, Integer, inspect
 
-from covidvaccinationproject.util.sqlconnector import SqlConnector
+from covidvaccinationproject.util.sqlconnector import SqlConnector, Table
 
 from covidvaccinationproject.util.logger import logconfig
 
@@ -12,18 +12,19 @@ logging.getLogger('covidvaccinationproject.util.sqlconnector').setLevel(logging.
 
 @pytest.fixture
 def connector():
-    return SqlConnector()
+    return SqlConnector('test.db')
 
 
 def test_connect_db():
-    connector = SqlConnector()
+    connector = SqlConnector('test.db')
 
     try:
         conn = connector._connect_engine()
         conn.connect()
         connection_success = True
-    except Exception:
+    except Exception as ex:
         connection_success = False
+        print(ex)
 
     assert connection_success
 
@@ -33,7 +34,9 @@ def test_create_table(connector):
                     'test_col_1': Column(Integer, primary_key=True),
                     'test_col_2': Column(String)}
 
-    connector.create_table(table_schema)
+    test_table = Table('test_table', connector.engine, table_schema)
+
+    test_table.create_table()
 
     assert inspect(connector.engine).has_table('test_table')
 
