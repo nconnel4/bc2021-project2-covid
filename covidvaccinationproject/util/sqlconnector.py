6c01df48a189960eed54747d29a47ad50e22418c
nconnel4 <nconnel4@hotmail.com>
2021-07-15 13:04:41 -0400

    Add insert, create, and drop functions
    Added functions to create table, drop table, and insert into table

--- covidvaccinationproject/util/sqlconnector.py
+++ covidvaccinationproject/util/sqlconnector.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import declarative_base
 import logging
 from pathlib import Path
@@ -6,15 +6,21 @@ from pathlib import Path
 
 class SqlConnector:
 
-    def __init__(self):
+    def __init__(self, db_name):
         self._logger = logging.getLogger(__name__)
+        self._db_name = db_name
 
-        self.engine = self._connect_engine()
+        self._engine = self._connect_engine()
+
+    @property
+    def engine(self):
+        return self._engine
 
     def _connect_engine(self):
         # get flask root level folder
+        engine = None
         root_path = Path(__file__).parent.parent
-        db_path = root_path / 'data/covid.db'
+        db_path = root_path / f'data/{self._db_name}'
 
         self._logger.info('Connecting to database')
         try:
@@ -24,8 +30,59 @@ class SqlConnector:
 
         return engine
 
-    def create_table(self, table_schema):
-        Base = declarative_base()
 
-        table = type('Table', (Base, ), table_schema)
-        table.__table__.create(bind=self.engine)
+class Table:
+
+    def __init__(self, table_name, engine, table_schema = None):
+        self._logger = logging.getLogger(__name__)
+        self._engine = engine
+
+        self._metadata = MetaData(bind=self._engine)
+        self._Base = declarative_base()
+
+        self._table_name = table_name
+        self._table_schema = table_schema
+
+        self._table = self._reflect_table_metadata()
+
+    @property
+    def table(self):
+        return self._table
+
+    def create_table(self):
+        """ Creates table if table does not exist in database """
+        if self._table is None:
+            table = type('Table', (self._Base, ), self._table_schema)
+            table.__table__.create(bind=self._engine)
+            self._logger.info('%s table created', self._table_name)
+
+            # update table object after creation
+            self._table = self._reflect_table_metadata()
+
+    def drop_table(self):
+        """ drops table if exists in database """
+        if self._table is not None:
+            self._table.drop()
+            self._logger.info('%s table dropped', self._table_name)
+
+    def insert_data(self, data_dict: list):
+        """ Inserts data into table. Data is passed as a list of dictionaries """
+        if self._table is None:
+            self._logger.error('%s table does not exist. Create table first')
+        else:
+            conn = self._engine.connect()
+            conn.execute(self._table.insert(), data_dict)
+
+    def _reflect_table_metadata(self):
+        """ Reflects database object if exists. Returns None if object does no exist """
+        self._metadata.reflect()
+        try:
+            table = self._metadata.tables[self._table_name]
+        except KeyError:
+            table = None
+        except Exception as ex:
+            self._logger.error(ex)
+            raise(ex)
+
+        self._logger.debug(table)
+        return table
