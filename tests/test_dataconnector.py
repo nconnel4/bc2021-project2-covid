dce9ad61d10d0747b02550328153faca40938e1b
nconnel4 <nconnel4@hotmail.com>
2021-07-21 09:53:27 -0400

    Add test for csv extraction

--- tests/test_dataconnector.py
+++ tests/test_dataconnector.py
@@ -20,3 +20,10 @@ def test_invalid_site_error():
     url = 'http://httpbin.org/status/404'
     with pytest.raises(dc.InvalidSiteError):
         data = dc.get_json_from_web(url)
+
+
+def test_connect_web_csv():
+    url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/variants/covid-variants.csv'
+    data = dc.get_csv_from_web(url)
+
+    assert data is not None
